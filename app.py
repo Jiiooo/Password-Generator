cbca01be43194b0444c62a7725454c9917c73c15
Jiiooo <69598451+Jiiooo@users.noreply.github.com>
2022-12-13 20:08:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,21 @@
 import streamlit as st
-import string
-import random
+import formula1 as f
 
 language = st.sidebar.selectbox(
     "Choose your language",
     ("English", "Indonesian")
 )
 
-
 if language == "English":
     st.title("Password Generator")
-
     st.write("This page is to help you generate random password")
-
+    
     option = st.selectbox(
     "Do you want to strengthen the previous password you have created or do you want to create a new password?",
     ("Strengthen the password that I have created", "Create a new password"))
     st.write("You selected:", option)
-    
-    if option ==  "Create a new password":
 
-        
+    if option ==  "Create a new password":
         length = st.slider("Password Length", 6,100)
 
         uppercase = st.checkbox("Uppercase Letters")
@@ -28,31 +23,14 @@ if language == "English":
         symbols = st.checkbox("Symbols")
         numbers = st.checkbox("Numbers")
 
-        def generate_password():
-            password = []
-            characters = []
-            if uppercase:
-                characters.extend(list(string.ascii_uppercase))
-            if lowecase:
-                characters.extend(list(string.ascii_lowercase))
-            if symbols:
-                characters.extend(list(string.punctuation))
-            if numbers:
-                characters.extend(list(string.digits))
-            while len(password) != length:
-                password.append(random.choice(characters))
-            return "".join(password)
-    
-        password = generate_password()
-        result=st.button("Make Password")
+        result=st.button("Make password")
         if result:
+            password = f.generate_password(length,uppercase,lowecase,symbols,numbers)
             st.write("Your password is:")
             st.success(password)
 
-    
     if option == "Strengthen the password that I have created":
         title = st.text_input("Enter the password that you have created")
-
         length = st.slider("Password Length", 1,100)
 
         uppercase = st.checkbox("Uppercase Letters")
@@ -60,35 +38,15 @@ if language == "English":
         symbols = st.checkbox("Symbols")
         numbers = st.checkbox("Numbers")
 
-        def generate_password():
-            password = []
-            characters = []
-            if uppercase:
-                characters.extend(list(string.ascii_uppercase))
-            if lowecase:
-                characters.extend(list(string.ascii_lowercase))
-            if symbols:
-                characters.extend(list(string.punctuation))
-            if numbers:
-                characters.extend(list(string.digits))
-            while len(password) != length:
-                password.append(random.choice(characters))
-            return "".join(password)
-    
-        password = generate_password()
-
-        password1 = title + password
-
         result=st.button("Make Password")
         if result:
+            password = f.generate_password(length,uppercase,lowecase,symbols,numbers)
+            password1 = title + password
             st.write("Your password is:")
             st.success(password1)
 
-        
-
 if language == "Indonesian":
     st.title("Pembuat Kata Sandi")
-
     st.write("Halaman ini untuk membantu Anda membuat kata sandi dengan acak")
 
     option = st.selectbox(
@@ -97,39 +55,22 @@ if language == "Indonesian":
     st.write("Kamu memilih:", option)
     
     if option ==  "Buat kata sandi baru":
-
-        
         length = st.slider("Panjang Kata Sandi", 6,100)
 
         uppercase = st.checkbox("Huruf Besar")
         lowecase = st.checkbox("Huruf Kecil")
         symbols = st.checkbox("Simbol")
         numbers = st.checkbox("Angka")
-        def generate_password():
-            password = []
-            characters = []
-            if uppercase:
-                characters.extend(list(string.ascii_uppercase))
-            if lowecase:
-                characters.extend(list(string.ascii_lowercase))
-            if symbols:
-                characters.extend(list(string.punctuation))
-            if numbers:
-                characters.extend(list(string.digits))
-            while len(password) != length:
-                password.append(random.choice(characters))
-            return "".join(password)
-    
-        password = generate_password()
+
         result=st.button("Buat Kata Sandi")
         if result:
+            password = f.generate_password(length,uppercase,lowecase,symbols,numbers)
             st.write("Kata Sandi Anda adalah:")
             st.success(password)
 
     
     if option == "Perkuat kata sandi yang telah saya buat":
         title = st.text_input("Masukkan kata sandi yang telah kamu buat ")
-
         length = st.slider("Panjang Kata Sandi", 1,100)
 
         uppercase = st.checkbox("Huruf Besar")
@@ -137,27 +78,9 @@ if language == "Indonesian":
         symbols = st.checkbox("Simbol")
         numbers = st.checkbox("Angka")
 
-
-        def generate_password():
-            password = []
-            characters = []
-            if uppercase:
-                characters.extend(list(string.ascii_uppercase))
-            if lowecase:
-                characters.extend(list(string.ascii_lowercase))
-            if symbols:
-                characters.extend(list(string.punctuation))
-            if numbers:
-                characters.extend(list(string.digits))
-            while len(password) != length:
-                password.append(random.choice(characters))
-            return "".join(password)
-    
-        password = generate_password()
-
-        password1 = title + password
-
         result=st.button("Buat Kata Sandi")
         if result:
-            st.write("Your password is:")
+            password = f.generate_password(length,uppercase,lowecase,symbols,numbers)
+            password1 = title + password
+            st.write("Kata sandi Anda adalah:")
             st.success(password1)
