36c055d3d25766b0a1b45847384ea1034ded7835
Jiiooo <69598451+Jiiooo@users.noreply.github.com>
2022-12-23 20:52:16 +0700

    Update EnglishFormula.py

--- EnglishFormula.py
+++ EnglishFormula.py
@@ -13,9 +13,9 @@ def generate_password(length,uppercase,lowecase,symbols,numbers):
         characters.extend(list(string.punctuation))
     if numbers:
         characters.extend(list(string.digits))
-        while len(password) != length:
-            password.append(random.choice(characters))
-        return "".join(password)
     if not uppercase and not lowecase and not symbols and not numbers:
         st.error("Program error! You must check at least one of the boxes above to create a password.")
+    while len(password) != length:
+        password.append(random.choice(characters))
+    return "".join(password)
 
